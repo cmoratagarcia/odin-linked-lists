5024884ea088cedfbdeab71b9949504744dc58d8
Carmen Morata <cmoratagarcia@gmail.com>
2025-02-27 11:03:09 -0800

    Update script in HTML and test as per requirements

--- main.js
+++ main.js
@@ -2,17 +2,10 @@ import LinkedList from "./src/LinkedList.js";
 
 const list = LinkedList();
 
-list.append(2);
-list.append("wow");
-list.prepend(666);
-list.prepend("finally");
-list.append("LAST");
-
-console.log(list.toString());
-
-list.insertAt("firstest", 0);
-list.insertAt("latest", 6);
-list.insertAt("middle", 3);
-console.log(list.toString());
-list.removeAt(0);
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
 console.log(list.toString());
